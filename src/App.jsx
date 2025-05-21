314d7490d6a44920da20c04b4ccd8e7555cb5f86
jakariya0dev <jhalhadi@gmail.com>
2025-05-21 13:43:25 +0600

    design updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+
 import Navbar from "./components/common/Navbar.jsx";
 import {Outlet} from "react-router";
 import Footer from "./components/common/Footer.jsx";
@@ -6,7 +6,6 @@ import {ToastContainer} from "react-toastify";
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -18,10 +18,15 @@ const Home = () => {
   return (
     <div>
       <Sliders />
-      <section className="max-w-7xl mx-auto pt-10 p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {upcomigGroups.map((group) => (
-          <GroupCard key={group._id} group={group} />
-        ))}
+      <section>
+        <h2 className="text-2xl font-bold text-center mb-6">
+          Discover New Hobbies
+        </h2>
+        <div className="max-w-7xl mx-auto pt-10 p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {upcomigGroups.map((group) => (
+            <GroupCard key={group._id} group={group} />
+          ))}
+        </div>
       </section>
       <Reviews />
       <Newsletter />

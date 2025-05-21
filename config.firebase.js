41a9dc30a5352a5c1ab5ecdb80e0261369003f4f
jakariya0dev <jhalhadi@gmail.com>
2025-05-21 12:32:15 +0600

    nav logo fixed

--- config.firebase.js
+++ config.firebase.js
@@ -1,18 +1,15 @@
-
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBolWUrrogMBriym-EraH2AOn-wUYnwo9U",
-    authDomain: "alpha-90050.firebaseapp.com",
-    databaseURL: "https://alpha-90050.firebaseio.com",
-    projectId: "alpha-90050",
-    storageBucket: "alpha-90050.firebasestorage.app",
-    messagingSenderId: "796323034333",
-    appId: "1:796323034333:web:29ecd31ae0c06e68f01734",
-    measurementId: "G-H41LQ9SJHE"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
-
-  const app = initializeApp(firebaseConfig);
-  export default app;
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -4,6 +4,7 @@ import { Link, NavLink } from "react-router";
 import { toast } from "react-toastify";
 import { Tooltip } from "react-tooltip";
 import app from "../../../config.firebase.js";
+import Hamburger from "../../assets/icons/Hamburger.jsx";
 import { AuthContext } from "./../../providers/AuthProvider.jsx";
 import LoaderDotted from "./LoaderDotted.jsx";
 
@@ -48,26 +49,12 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="sticky top-0 z-50 bg-base-100">
+    <nav className="sticky top-0 z-50 bg-base-100 px-4">
       <div className="navbar bg-base-100 shadow-sm">
         <div className="navbar-start">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                {" "}
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />{" "}
-              </svg>
+              <Hamburger />
             </div>
             <ul
               tabIndex={0}
@@ -76,7 +63,9 @@ const Navbar = () => {
               {links}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">daisyUI</a>
+          <Link to="/" className="btn-ghost text-xl font-bold">
+            Hobby<span className="text-amber-500">Hub</span>
+          </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">{links}</ul>
